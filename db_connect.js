577d43e9129aa56dfbf5aed2f1d4f4d97ea00ce7
Mandeep Singh <95164601+Mandeep28@users.noreply.github.com>
2023-02-23 21:25:51 +0530

    Update Db_connect.js

--- db_connect.js
+++ db_connect.js
@@ -1,13 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
-const url = "mongodb+srv://mandeep28:myDATAisMINE28@cluster0.prrizft.mongodb.net/inotebook?retryWrites=true&w=majority";
-// const url = "mongodb://127.0.0.1:27017/test";
-
-
-// mongoose.connect(url, ()=>{
-//   console.log("Connect ToDB.....");
-  
-// });
 
 
 // connect to mongo DB
