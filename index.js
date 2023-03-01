4d399f2549e6bf3395bab0910ba756126729325d
mandeep28 <mandeepsingh13524@gmail.com>
2023-03-01 18:32:31 +0530

    Update the index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 //INTERNAL PACKAGES (modules)
 const auth = require("./routes/auth");
 const notes = require("./routes/notes");
+// const admin = require("./routes/admin")
 const connectDB = require("./db_connect");
 const errorHandler = require("./middleware/errorHandler")
 const cors = require("cors");
@@ -16,17 +17,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 5000;
-// const url = process.env.MONGO_URI;
-const url = "mongodb://127.0.0.1:27017/magicNotes";
+const url = process.env.MONGO_URI;
+// const url = "mongodb://127.0.0.1:27017/magicNotes";
 
 // routes
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/notes", notes);
-app.get("/onlyforcheck",(req, res)=>{
-    res.status(200).json({status: true, msg : "yeah its working fine"});
-})
-app.use(errorHandler)
-
+// app.use("api/v1/admin", admin);
 
 
 
